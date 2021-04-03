cb58e00ceed6bc0e310eb8df525bd2f672d9932d
okucuk <kucukomer@gmail.com>
2021-04-04 00:13:52 +0300

    supplier names counted

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-import datetime
+import openpyxl
 
-user_input = input("Enter your goal with a deadline separated by colon\n")
-input_list = user_input.split(":")
+inv_file = openpyxl.load_workbook("inventory.xlsx")
+product_list = inv_file["Sheet1"]
 
-goal = input_list[0]
-deadline = input_list[1]
+#dictionary created for suppliers and products
+products_per_supplier = {}
+#print(product_list.max_row)
 
-print(datetime.datetime.strptime(deadline, "%d.%m.%Y"))
-print(type(datetime.datetime.strptime(deadline, "%d.%m.%Y")))
+#range default starts from 0, last is exclusive
+for product_row in range(2, product_list.max_row+1):
+    supplier_name = product_list.cell(product_row, 4).value
+
+    if supplier_name in products_per_supplier:
+        current_num_products = products_per_supplier[supplier_name]
+        products_per_supplier[supplier_name] = current_num_products + 1
+    else:
+        print("adding a mew supplier")
+        products_per_supplier[supplier_name] = 1
+
+print(products_per_supplier)
