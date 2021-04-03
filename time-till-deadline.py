449f1b3355c06a0950a40cfbe53a7b6f74cbb870
okucuk <kucukomer@gmail.com>
2021-04-03 16:49:57 +0300

    updated

--- time-till-deadline.py
+++ time-till-deadline.py
@@ -6,10 +6,13 @@ input_list = user_input.split(":")
 goal = input_list[0]
 deadline = input_list[1]
 
-print(datetime.datetime.strptime(deadline, "%d.%m.%Y"))
-print(type(datetime.datetime.strptime(deadline, "%d.%m.%Y")))
+"""splitted list elements  are always strings. we need to convert it to date element"""
+#print(datetime.datetime.strptime(deadline, "%d.%m.%Y"))
+#print(type(datetime.datetime.strptime(deadline, "%d.%m.%Y")))
 
 deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%Y")
 today_date = datetime.datetime.today()
+time_till = deadline_date - today_date
 
-print(deadline_date - today_date)
+#Need f to use variables in printed text. conversion from float to int is below
+print(f"Dear Omer, Time remaining to {goal} is {int(time_till.total_seconds()/60/60)} hours")
